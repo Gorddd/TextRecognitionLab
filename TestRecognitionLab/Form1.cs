95a91b89087b8936f752efdd602379e49db49e81
Gorddd <kasual2014@icloud.com>
2023-10-23 21:16:42 +0300

    Add project files.

---
+++ TestRecognitionLab/Form1.cs
@@ -0,0 +1,46 @@
+﻿using IronOcr;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestRecognitionLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            pictureBox1.Image = Image.FromFile(openFileDialog.FileName);
+
+            ProcessImage(openFileDialog.FileName);
+        }
+
+        private void ProcessImage(string fileName)
+        {
+            var ocr = new IronTesseract();
+            ocr.Language = OcrLanguage.English;
+            ocr.Configuration.TesseractVersion = TesseractVersion.Tesseract5;
+
+            using (var Input = new OcrInput())
+            {
+                Input.AddImage(fileName);
+                var result = ocr.Read(Input);
+                textBox1.Text = result.Text;
+            }
+        }
+    }
+}
